31d252508e616c767d8b46a77b043cefe4d8c6df
aryan sharma <127038461+Aryan001521@users.noreply.github.com>
2026-03-08 18:33:36 +0530

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -558,7 +558,7 @@ if st.button("Predict"):
 else:
     st.markdown(
         '<div class="card"><span class="small">Click <b>Predict</b> to see the result.</span></div>',
-        unsafe_allow_html=True
-      st.write("HF token found:", "HF_TOKEN" in st.secrets)
+        unsafe_allow_html=True,
+      st.write("HF token found:", "HF_TOKEN" in st.secrets),
       st.write("Repo ID:", HF_REPO_ID)
     )
