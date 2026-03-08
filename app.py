386621540adbc601da86cf67da281ef3029273d1
aryan sharma <127038461+Aryan001521@users.noreply.github.com>
2026-03-08 18:35:48 +0530

    Remove debug output for HF token and Repo ID
    Removed debug information related to HF token and Repo ID.

--- app.py
+++ app.py
@@ -558,7 +558,5 @@ if st.button("Predict"):
 else:
     st.markdown(
         '<div class="card"><span class="small">Click <b>Predict</b> to see the result.</span></div>',
-        unsafe_allow_html=True,
-      st.write("HF token found:", "HF_TOKEN" in st.secrets),
-      st.write("Repo ID:", HF_REPO_ID),
+        unsafe_allow_html=True
     )
